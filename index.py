90ff0e5d7153da19eb0a41a999099b343730a3a4
Zoltan Ver <zoltan.ver@ticketmaster.co.uk>
2019-08-06 18:08:51 +0100

    changing API endpoint stage name #2

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ DUBLAB_API_EP = "https://api-1.dublab.com/wp-json/lazystate/v1"
 
 def handler(event, context):
     if type(event) != type(None):
-        path = str(event['path'])
+        path = str('/archive/' + event['path'])
     else:
         path = "/archive/ale-elevation-through-sound-live-from-tunein-studios-11-18-15"
     r = requests.get(''.join([DUBLAB_API_EP, path]))
