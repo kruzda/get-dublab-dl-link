f49fd2c8bfdc764285b94233e182e9c8cd944093
Zoltan Ver <zoltan.ver@ticketmaster.co.uk>
2019-08-06 17:42:27 +0100

    event object may be Null

--- index.py
+++ index.py
@@ -3,7 +3,10 @@ from botocore.vendored import requests
 DUBLAB_API_EP = "https://api-1.dublab.com/wp-json/lazystate/v1"
 
 def handler(event, context):
-    path = str(event['path'])
+    if type(event) != NoneType:
+        path = str(event['path'])
+    else:
+        path = "/archive/ale-elevation-through-sound-live-from-tunein-studios-11-18-15"
     r = requests.get(''.join([DUBLAB_API_EP, path]))
     link = r.json()[path]['audio']['url']
     return {
